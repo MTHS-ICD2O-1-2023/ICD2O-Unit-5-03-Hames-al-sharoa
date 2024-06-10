44361963c76c20d9714910a9098aba8d47db807b
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-06-10 17:52:19 +0000

    save al changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 hames al sharoa All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: hames al sharoa
+// Created on: may 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-03-JS/sw.js", {
+    scope: "/ICS2O-Unit5-03-JS/",
+  })
+}
+
+/**
+ * This function displays the user's movie age rating.
+ */
+function ageRating() {
+  // input
+  const age = parseInt(document.getElementById("age").value)
+
+  // process
+  if (age >= 17) {
+    document.getElementById("watchable").innerHTML =
+      "You're old enough to watch any R movie alone. Congrats!"
+  } else if (age >= 13) {
+    document.getElementById("watchable").innerHTML =
+      "You can watch PG-13 movies alone."
+  } else if (age >= 5) {
+    document.getElementById("watchable").innerHTML =
+      "You're pretty young... let's stick to G or PG for now."
+  } else {
+    document.getElementById("watchable").innerHTML =
+      "I'm surprised you even found this site... shouldn't you be napping?"
+  }
+}
